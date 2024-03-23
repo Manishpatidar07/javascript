e97760dd77461a3d99b65a88a649954e3f21f810
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-23 08:46:50 +0000

    convwesion of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+//let score = "33abs"
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber );
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1 , false = 0
+
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true , 0 = false
+// "" = false
+// "manish" = true
+
+let someNumber = 43
+let stringsomeNumber = String(someNumber)
+console.log( typeof stringsomeNumber);
