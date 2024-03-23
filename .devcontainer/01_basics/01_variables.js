8992ba86e5c69c753f60be4dcaf9ecc2f0d264df
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-23 07:54:44 +0000

    my first commit

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "23432"
+let accountemail ="manish@gmail.com"
+var accountpassword = "72634"
+accountcity ="jaora"
+
+accountid=2
+accountemail="manish12@gmail.com"
+accountpassword= "23453"
+accountcity="ratlam"
+
+/*
+prefer not to use var
+beacause  of issue in block scop and functional scope
+*/
+console.table([accountemail,accountpassword,accountcity]);
+
