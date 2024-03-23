0ae12f52b4c16881f6f627f829cfb1eec84fb994
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-23 09:29:17 +0000

    conversion of datatype

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -12,7 +12,7 @@
 
 let IsLoggedIn = 1
 let booleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 = true , 0 = false
 // "" = false
@@ -20,4 +20,39 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 43
 let stringsomeNumber = String(someNumber)
-console.log( typeof stringsomeNumber);
+//console.log( typeof stringsomeNumber);
+
+
+//***********************************operation***************************************
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2/3);
+//console.log(2%3);
+//console.log(2**3);
+
+let str1 = "hello"
+let str2 = "manish"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log(1 + 2);
+//console.log("1" + 2);
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//console.log(1 + 2 + 3 + 5);
+
+let gameCounter =  100
+++gameCounter
+console.log(gameCounter);
+
+// prefix and postfix me pdna
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
