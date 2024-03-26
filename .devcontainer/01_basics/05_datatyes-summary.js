52ad665a30132760d185b1a315ee2ee923e6bebf
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-26 06:47:28 +0000

    datatype summary

---
+++ .devcontainer/01_basics/05_datatyes-summary.js
@@ -0,0 +1,18 @@
+// primitive datatype
+// 7 types of DT
+// String, number, boolean, null, undefined , symbol , bigInt
+
+//Reference (Non primitive)
+//Array , objects, function
+
+const heros = ["Akshay" , "Ajay"  , "Raju"]; //Array Example
+
+
+let Myobj = {     //object example
+    name: "manish",
+    age: 22,
+}
+
+const myFunction = function(){
+console.log("Hello world");
+}
