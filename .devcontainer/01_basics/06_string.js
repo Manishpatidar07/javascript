6afa910d21a9156f2abbe4af703f203e888999cd
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-28 06:22:24 +0000

    string

---
+++ .devcontainer/01_basics/06_string.js
@@ -0,0 +1,12 @@
+const name = "manish"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+console.log( `hello my name is ${name} and is my repo ${repoCount}` );
+
+const gameName = new String('manishpatidar')
+console.log(gameName);
+console.log(gameName.toUpperCase());
+
+
