b2c3e0a97cf6b425acc58b2a8faa95e7bfe26179
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-03-28 06:22:56 +0000

    number and maths

---
+++ .devcontainer/01_basics/07_number_and_maths.js
@@ -0,0 +1,33 @@
+//++++++++++++++++Number++++++++++++++++++
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8976
+//console.log(otherNumber.toPrecision(4));
+
+
+//+++++++++++++++++++++++maths++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(6.7));
+
+
+
+console.log(Math.random());
+console.log((Math.random ) * 10) ;
+console.log((Math.random ()*10)+ 1) ;
+
+min = 10 
+max = 20
+
+console.log(Math.random() * (max - min +1) );
+console.log(Math.floor(Math.random() * (max - min +1) + min)); //imp concect and formula
