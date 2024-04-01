d55f2fa9d4cb8fd42ec28ab7428d91c399b784ca
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-04-01 09:34:05 +0000

    arrays

---
+++ .devcontainer/01_basics/02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//array
+
+
+const myArr = [0, 1, 2, 3, 4, 5, true, "manish"]
+ 
+const Arr2 = new Array(1, 2, 3, 4, 5)
+//console.log(myArr[0]); // 0 se start hogi
+
+//Array mathods
+
+myArr.push(6)
+myArr.pop()
+console.log(myArr);
