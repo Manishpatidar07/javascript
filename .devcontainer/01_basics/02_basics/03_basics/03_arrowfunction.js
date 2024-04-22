39f96dd2a0b9e18167fd1c6153a5e25676fa96e4
Manishpatidar07 <130732353+Manishpatidar07@users.noreply.github.com>
2024-04-22 07:28:56 +0000

    arrow

---
+++ .devcontainer/01_basics/02_basics/03_basics/03_arrowfunction.js
@@ -0,0 +1,5 @@
+// const addTwo = (num1,num2) =>{            //explicit return
+//     return num1 + num2
+// }
+const addTwo = (num1,num2) => (num1 + num2)     //imlicit return
+console.log(addTwo(3,4))
